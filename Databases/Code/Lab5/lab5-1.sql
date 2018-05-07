737daed0b524072d5cbe6db07b1ef87098088c1c
Lucian Boaca <lucianboaca97@gmail.com>
2018-05-07 14:06:34 +0300

    Finished lab 5

--- Databases/Code/Lab5/lab5-1.sql
+++ Databases/Code/Lab5/lab5-1.sql
@@ -1,25 +1,110 @@
-select department_name, sum(salary), round(avg(salary), 2)
-from departments d, employees e
-where d.department_id = e.department_id
-group by department_name
-having sum(salary) > (
-    select avg(salary)
-    from departments
-);
+--Lucian Boaca
+--Grupa 241
+--1
+-- with, varianta 1
+with
+dept_costuri as (
+    select  department_name, sum(salary) dept_cost
+    from    employees e, departments d
+    where   e.department_id = d.department_id
+    group   by department_name
+)
+select  * 
+from    dept_costuri
+where   dept_cost > (
+    select avg(dept_cost) from dept_costuri
+)
+order   by dept_cost;
 
-SELECT *
-FROM (
-    SELECT last_name, job_id, salary
-    FROM employees
-    ORDER BY salary DESC
+-- with, varianta 2
+with
+dept_costuri as (
+    select  department_name, sum(salary) dept_cost
+    from    employees e, departments d
+    where   e.department_id = d.department_id
+    group   by department_name
+),
+medie_cost as (
+    select  avg(dept_cost) medie
+    from    dept_costuri
+)
+select  *
+from    dept_costuri, medie_cost
+where   dept_cost > medie
+order   by dept_cost;
+
+--2
+select  department_name, sum(salary) dept_cost
+from    employees e, departments d
+where   e.department_id = d.department_id
+group   by department_name
+having  sum(salary) > (
+    select  avg(cst)
+    from (
+        select  sum(salary) cst
+        from    employees
+        group   by department_id
     )
-WHERE ROWNUM <= 5;
+)
+order   by dept_cost;
 
+--3
+select  department_name, sum(salary) dept_cost
+from    employees e, departments d
+where   e.department_id = d.department_id
+group   by department_name
+having  sum(salary) > (
+    select  avg(cst)
+    from (
+        select  sum(salary) cst
+        from    employees
+        where   department_id is not null
+        group   by department_id
+    )
+)
+order   by dept_cost;
+
+--4
+-- with, varianta 1
+with
+dept_costuri as (
+    select  department_name, sum(salary) dept_cost
+    from    employees e, departments d
+    where   e.department_id = d.department_id(+)
+    group   by department_name
+)
+select  * 
+from    dept_costuri
+where   dept_cost > (
+    select avg(dept_cost) from dept_costuri
+)
+order   by dept_cost;
+
+-- with, varianta 2
+with
+dept_costuri as (
+    select  department_name, sum(salary) dept_cost
+    from    employees e, departments d
+    where   e.department_id = d.department_id(+)
+    group   by department_name
+),
+medie_cost as (
+    select  avg(dept_cost) medie
+    from    dept_costuri
+)
+select  *
+from    dept_costuri, medie_cost
+where   dept_cost > medie
+order   by dept_cost;
+
+--5
+-- gresita
 select rownum, last_name, salary
 from employees
 where rownum <= 5
 order by salary desc;
 
+-- varianta 1
 select *
 from (
     select last_name, job_id, salary
@@ -27,6 +112,15 @@ from (
     order by salary desc
 )
 where rownum <= 5;
+
+-- varianta 2
+select last_name, job_id, salary
+from employees e
+where 5 > (
+    select count(*)
+    from employees
+    where salary > e.salary
+) order by salary desc;
 
 
 select rownum, last_name, salary
@@ -37,31 +131,79 @@ select rownum, last_name, salary
 from employees
 where rownum between 3 and 5;
 
-select last_name, job_id, salary
-from employees e
-where 5 > (
-    select count(*)
-    from employees
-    where salary > e.salary
-) order by salary desc;
+--6
+select  * 
+from (
+    select  department_name, count(*) nr_ang
+    from    employees e, departments d
+    where   e.department_id = d.department_id(+)
+    group   by department_name
+    order   by nr_ang desc
+)
+where   rownum <= 5;
 
+--7
+select  *
+from (
+    select  job_title, (min_salary + max_salary) / 2 medie
+    from    jobs
+    order   by medie asc
+)
+where   rownum <= 3;
+
+select  *
+from (
+    select  job_title, avg(salary) medie
+    from    employees e, jobs j
+    where   e.job_id = j.job_id
+    group   by job_title
+    order   by medie asc
+)
+where   rownum <= 3;
 
 --8
-SELECT last_name, salary, job_id, department_id
-FROM
-employees
-WHERE (department_id, salary) IN (SELECT department_id, MIN(salary)
-FROM
-employees
-GROUP BY department_id);
+--fara sincronizare
+select  last_name, salary, job_id, department_id
+from    employees
+where   (department_id, salary) in (
+    select  department_id, min(salary)
+    from    employees
+    group   by department_id
+)
+order   by salary;
 
+--cu sincronizare
+select  last_name, salary, job_id, department_id
+from    employees e
+where   salary = (
+    select  min(salary)
+    from    employees
+    where   department_id = e.department_id
+)
+order   by salary;
 
-SELECT last_name, salary, job_id, department_id
-FROM
-employees e
-WHERE salary = (SELECT MIN(salary)
-                FROM employees
-                WHERE department_id=e.department_id);
+--9
+--fara sincronizare
+select  first_name, last_name, salary
+from    employees
+where   (job_id, salary) in (
+    select  job_id, max(salary)
+    from    employees
+    group   by job_id
+)
+order   by salary;
+
+--cu sincronizare
+select  first_name, last_name, salary
+from    employees e
+where   salary = (
+    select  max(salary)
+    from    employees
+    where   job_id = e.job_id
+)
+order   by salary;
+
+--10
 
 --11
 select department_id, department_name 
@@ -79,6 +221,77 @@ from departments
 --where department_id not in (10, null);
 where department_id <> 10 and department_id <> null;
 
+--12
+select  *
+from    employees
+where   employee_id not in (
+    select  manager_id
+    from    employees
+    where   manager_id is not null
+);
+
+--13
+select  * 
+from    employees
+where   salary > all (
+    select  salary
+    from    employees
+    where   job_id like '%CLERK'
+);
+
+--14
+select  first_name, last_name, salary
+from    employees
+where   salary > all (
+    select  avg(salary)
+    from    employees
+    group   by department_id
+);
+
+select  first_name, last_name, salary
+from    employees
+where   salary > (
+    select  max(slr)
+    from    (
+        select  avg(salary) slr
+        from    employees
+        group   by department_id
+    )
+);
+
+--15
+select  first_name, last_name, department_id
+from    employees
+where   (department_id, hire_date) in (
+    select  department_id, min(hire_date)
+    from    employees
+    group   by department_id
+);
+
+--17
+select  * from job_grades;
+
+with lowest as (
+    select  lowest_sal lowest
+    from    job_grades
+    where   grade_level = 1
+),
+highest as (
+    select  highest_sal highest
+    from    job_grades
+    where   grade_level = 1
+),
+depts as (
+    select  department_id
+    from    employees
+    where   department_id is not null
+    and     salary >= (select * from lowest)
+    and     salary <= (select * from highest)
+    group   by department_id
+    having  count(department_id) >= 2)
+select  first_name, last_name, department_id
+from    employees
+where   department_id in (select * from depts);
 
 --18
 select count(*) from employees
@@ -89,10 +302,95 @@ select count(*) nr_total, sum(decode(to_char(hire_date, 'YYYY'), 1997, 1, 0)) "n
 from employees;
 
 
+--19
+select  department_id, job_id, round(avg(salary))
+from    employees
+group   by department_id, job_id
+union
+select  department_id, null as job_id, round(avg(salary))
+from    employees
+group   by department_id, job_id
+union
+select  null as department_id, job_id, round(avg(salary))
+from    employees
+group   by department_id, job_id
+union
+select  null as department_id, null as job_id, round(avg(salary))
+from    employees
+order   by job_id;
 
+--20
+select * from job_history;
 
+with
+job_data as (
+    select  employee_id, hire_date start_date, department_id, job_id
+    from    employees
+    union
+    select  employee_id, start_date, department_id, job_id
+    from    job_history
+)
+select  employee_id, job_title, department_name, start_date
+from    job_data jd, departments d, jobs j
+where   jd.department_id = d.department_id
+and     jd.job_id = j.job_id;
 
+--21
+select  employee_id, last_name
+from    employees
+where   salary < 3000
+intersect
+select  employee_id, last_name
+from    employees
+where   department_id = 50;
 
+--22
+with 
+ppl as (
+    select  employee_id, job_id, department_id
+    from    employees
+    intersect
+    select  employee_id, job_id, department_id
+    from    job_history
+)
+select  employee_id, job_title, department_name
+from    ppl pp, departments d, jobs j
+where   pp.department_id = d.department_id
+and     pp.job_id = j.job_id;
 
+--23
+with 
+ppl as (
+    select  employee_id, job_id, department_id
+    from    employees
+    intersect
+    select  employee_id, job_id, department_id
+    from    job_history
+)
+select  first_name, last_name, job_title, department_name
+from    ppl pp, departments d, jobs j, employees e
+where   pp.department_id = d.department_id
+and     pp.job_id = j.job_id
+and     pp.employee_id = e.employee_id;
 
+with 
+ppl as (
+    select  employee_id, first_name, last_name, job_id, department_id
+    from    employees
+    intersect
+    select  e.employee_id, first_name, last_name, jh.job_id, jh.department_id
+    from    job_history jh, employees e
+    where   e.employee_id = jh.employee_id
+)
+select  first_name, last_name, job_title, department_name
+from    ppl pp, departments d, jobs j
+where   pp.department_id = d.department_id
+and     pp.job_id = j.job_id;
+
+--24
+select  department_id
+from    departments
+minus
+select  distinct department_id
+from    employees;
 
---
+++ Databases/Code/Lab5/lab5-2.sql
@@ -0,0 +1,414 @@
+--Lucian Boaca
+--Grupa 241
+--1
+--v1
+select  a.employee_id, a.last_name
+from    employees a, employees b
+where   a.salary > b.salary
+and     b.employee_id = 200;
+
+--v2
+select  employee_id, last_name
+from    employees e
+where   exists (
+    select  1
+    from    employees
+    where   employee_id = 200
+    and     e.salary > salary
+);
+
+--2
+with
+sal_200 as (
+    select  salary
+    from    employees
+    where   employee_id = 200
+)
+select  employee_id, last_name
+from    employees
+where   salary > (select * from sal_200);
+
+--3
+select  department_id, department_name
+from    departments d
+where   exists (
+    select  'x'
+    from    employees
+    where   department_id = d.department_id
+);
+
+--4
+with
+depts_with_people as (
+    select  distinct department_id
+    from    employees
+    where   department_id is not null
+)
+select  department_id, department_name
+from    departments
+where   department_id in (select * from depts_with_people);
+
+--5
+select  department_id, department_name
+from    departments d
+where   not exists (
+    select  1
+    from    employees
+    where   department_id = d.department_id
+);
+
+--6
+--a
+select  location_id, city
+from    locations
+where   location_id not in (
+    select  distinct location_id
+    from    departments d
+);
+--b
+select  location_id, city
+from    locations
+minus
+select  l.location_id, city
+from    locations l, departments d
+where     d.location_id = l.location_id;
+--c
+select  location_id, city
+from    locations l
+where   not exists (
+    select  1
+    from    departments d
+    where   d.location_id = l.location_id
+);
+--d
+select  l.location_id, city
+from    locations l full outer join departments d
+on      l.location_id = d.location_id
+where   d.department_id is null;
+
+select  l.location_id, city
+from    locations l, departments d
+where   l.location_id = d.location_id(+)
+and     d.location_id is null;
+
+--7
+select  employee_id, first_name, last_name
+from    employees e
+where   not exists (
+    select  project_id
+    from    work
+    where   employee_id = 202
+    minus
+    select  project_id
+    from    work
+    where   employee_id = e.employee_id
+);
+
+--8
+select  employee_id, first_name, last_name
+from    employees e
+where   not exists (
+    select  project_id
+    from    work
+    where   employee_id = e.employee_id
+    minus
+    select  project_id
+    from    work
+    where   employee_id = 202
+);
+
+--9
+select  employee_id, first_name, last_name
+from    employees e
+where   not exists (
+    select  project_id
+    from    work
+    where   employee_id = e.employee_id
+    minus
+    select  project_id
+    from    work
+    where   employee_id = 202
+)
+and     not exists (
+    select  project_id
+    from    work
+    where   employee_id = 202
+    minus
+    select  project_id
+    from    work
+    where   employee_id = e.employee_id
+);
+
+--10
+--v1
+select  p.project_id, project_name
+from    projects p, work w
+where   p.project_id = w.project_id
+and     employee_id in (
+    select  employee_id
+    from    employees
+    where   department_id = 20
+)
+group   by p.project_id, project_name
+having  count(*) = (
+    select  count(*)
+    from    employees
+    where   department_id = 20
+);
+
+--v2
+select  distinct p.project_id, project_name
+from    projects p, work w
+where   p.project_id = w.project_id
+and     not exists (
+    select  'x'
+    from    employees e
+    where   department_id = 20
+    and     not exists (
+        select  'x'
+        from    work w1
+        where   e.employee_id = w1.employee_id
+        and     w.project_id = w1.project_id
+    )
+);
+
+--11
+with
+proiecte_an as (
+    select  project_id
+    from    projects 
+    where   to_char(start_date, 'yyyy') = 1999
+)
+select  e.employee_id, e.first_name, e.last_name
+from    employees e
+where   not exists (
+    select * from proiecte_an
+    minus
+    select  project_id
+    from    work
+    where   employee_id = e.employee_id
+);
+
+--12
+select  level, employee_id, last_name, manager_id
+from    employees
+connect by prior manager_id = employee_id;
+
+--13
+select  level, employee_id, last_name, manager_id
+from    employees
+start   with employee_id = 103
+connect by prior manager_id = employee_id;
+
+--14
+select  level, employee_id, last_name, manager_id
+from    employees
+start   with employee_id = 103
+connect by manager_id = prior employee_id;
+
+--15
+with 
+id_min_sal as (
+    select  *
+    from (
+        select  employee_id
+        from    employees
+        order   by salary
+    )
+    where   rownum = 1
+)
+select  level, employee_id, last_name, manager_id
+from    employees
+start   with employee_id = (select * from id_min_sal)
+connect by prior manager_id = employee_id;
+
+--16
+select  level, employee_id, last_name, salary, manager_id
+from    employees
+start   with employee_id = 206
+connect by prior manager_id = employee_id;
+
+select  level, employee_id, last_name, salary, manager_id
+from    employees
+where   salary > 15000
+start   with employee_id = 206
+connect by prior manager_id = employee_id;
+
+select  level, employee_id, last_name, salary, manager_id
+from    employees
+start   with employee_id = 206
+connect by prior manager_id = employee_id 
+and     salary > 15000;
+
+--17
+--a
+--v1
+with 
+ref_id as (
+    select  employee_id
+    from    employees
+    where   upper(last_name) = upper('de haan')
+)
+select  employee_id, last_name, hire_date, salary, manager_id
+from    employees
+where   manager_id = (select * from ref_id);
+
+--v2
+select  employee_id, last_name, hire_date, salary, manager_id
+from    employees
+where   level = 2
+start   with upper(last_name) = upper('de haan')
+connect by manager_id = prior employee_id;
+
+--b
+select  level, employee_id, last_name, hire_date, salary, manager_id
+from    employees
+where   level = 2
+start   with upper(last_name) = upper('de haan')
+connect by manager_id = prior employee_id;
+
+--c
+select  level, employee_id, last_name, hire_date, salary, manager_id
+from    employees
+start   with upper(last_name) = upper('de haan')
+connect by manager_id = prior employee_id;
+
+--d
+--v1
+with 
+subalterni as (
+    select  employee_id
+    from    employees
+    where   level <> 1
+    start   with upper(last_name) = upper('de haan')
+    connect by manager_id = prior employee_id
+)
+select  employee_id, last_name, hire_date, salary, manager_id
+from    employees
+where   manager_id in (select * from subalterni);
+
+--v2
+select  level, employee_id, last_name, hire_date, salary, manager_id
+from    employees
+where   level > 2
+start   with upper(last_name) = upper('de haan')
+connect by manager_id = prior employee_id;
+
+--e
+select  level, employee_id, last_name, hire_date, salary, manager_id
+from    employees
+start   with upper(last_name) = upper('hunold')
+connect by prior manager_id = employee_id;
+
+--f
+select  level, employee_id, last_name, hire_date, salary, manager_id
+from    employees
+where   level = 2
+start   with upper(last_name) = upper('hunold')
+connect by prior manager_id = employee_id;
+
+--18
+select  count(*)
+from    employees
+where   level <> 1
+start   with employee_id = 107
+connect by prior manager_id = employee_id;
+
+--19
+with
+dept_mgrs as (
+    select  manager_id
+    from    departments
+    where   manager_id is not null
+)
+select  level, employee_id, last_name, hire_date, salary, manager_id
+from    employees
+start   with employee_id in (select * from dept_mgrs)
+connect by prior manager_id = employee_id;
+
+--20
+select  level, employee_id, last_name, manager_id 
+from    employees
+start   with manager_id is null
+connect by manager_id = prior employee_id;
+
+--21
+with
+dept_mgrs as (
+    select  manager_id
+    from    departments
+    where   manager_id is not null
+)
+select  level, employee_id, last_name, department_id
+from    employees
+where   employee_id in (select * from dept_mgrs)
+start   with manager_id is null
+connect by manager_id = prior employee_id;
+
+--22
+select  employee_id, manager_id, level, last_name, 
+        lpad(last_name, length(last_name) + level * 2 - 2, '_') nume
+from    employees
+start   with employee_id = 206
+connect by employee_id = prior manager_id;
+
+--23
+select  lpad(' ', 3 * level - 3) || last_name nume, level,
+        employee_id, manager_id, department_id
+from    employees
+start   with manager_id is null
+connect by prior employee_id = manager_id;
+
+--24
+--a
+select  lpad(' ', 3 * level - 3) || last_name nume, level,
+        employee_id, manager_id, department_id
+from    employees
+where   upper(last_name) <> upper('de haan')
+start   with manager_id is null
+connect by prior employee_id = manager_id;
+
+--b
+select  lpad(' ', 3 * level - 3) || last_name nume, level,
+        employee_id, manager_id, department_id
+from    employees
+start   with manager_id is null
+connect by prior employee_id = manager_id
+and     upper(last_name) <> upper('de haan');
+
+--25
+with
+emp_sk as (
+    select  employee_id, hire_date
+    from    employees
+    where   manager_id = (
+        select  employee_id
+        from    employees
+        where   initcap(last_name) = 'King'
+        and     initcap(first_name) = 'Steven'
+    )
+)
+select  employee_id, initcap(first_name) || ' ' || upper(last_name)
+        job_id, hire_date, manager_id
+from    employees
+where   to_char(hire_date, 'yyyy') != 1970
+start   with employee_id in (
+            select  employee_id
+            from    emp_sk
+            where   hire_date = (
+                    select  min(hire_date)
+                    from    emp_sk
+            )
+)
+connect by prior employee_id = manager_id;
+
+--26
+select  'true'
+from    job_grades
+having  min(highest_sal) < 10000;
+
+
+
+
+
+
